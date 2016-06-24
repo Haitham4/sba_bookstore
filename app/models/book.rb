f7be0ed1188a5ab02a4aa354ca2ae712d749feb3
Haitham Fallatah <Haitham.Fallatah.1990@gmail.com>
2016-06-24 11:33:27 -0400

    Added bonsai addon and gems to allow for elasticserach to work on heroku

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,3 +1,14 @@
+require 'elasticsearch/model'
+
 class Book < ActiveRecord::Base
   searchkick
+
+  include Elasticsearch::Model
+  include Elasticsearch::Model::Callbacks
+
 end
+
+
+Book.import
+
+@books = Book.search('Test').records
